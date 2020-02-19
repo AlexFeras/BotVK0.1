b9b2c0ce102af8900a20528ba183df7e20dc73c5
AlexFeras <alex.feras@bk.ru>
2020-02-19 20:45:07 +0300

    Бот 0,1 1234

--- Base.py
+++ Base.py
@@ -1,15 +1,5 @@
 import pandas as pd
 from datetime import datetime
-import os
-def getpicture(user_id,ball):
-    users = pd.read_csv('users.csv', sep=';', encoding='windows-1251', engine='python')
-    #teams = pd.read_csv('team.csv', sep=';', encoding='windows-1251', engine='python')
-    print(users.head())
-    users.loc[users['id']==user_id, 'баллы']+=ball
-    print(users.head())
-    users.to_csv('users.csv', sep=';', encoding='windows-1251')
-
-
 
 def day_t(): # передает актуальный день
     today = datetime.today()
@@ -17,12 +7,50 @@ def day_t(): # передает актуальный день
     #print (days)
     return days
 
+def hour_t(): # передает актуальный день
+    today = datetime.today()
+    hour=today.hour*60+today.minute
+    #print (days)
+    return hour
+
+def Data(id):
+    users = pd.read_csv('users.csv', sep=';', encoding='windows-1251', engine='python')
+    return users[users['id'] == id]['дата'].iloc[0]
+
+def Debt(id):
+    users = pd.read_csv('users.csv', sep=';', encoding='windows-1251', engine='python')
+    return users[users['id'] == id]['долги'].iloc[0]
+
+
+
+def Counter_d():
+    users = pd.read_csv('users.csv', sep=';', encoding='windows-1251', engine='python')
+    users.loc[users['дата'] != day_t(), 'долги'] += 1
+    users = users.drop('Unnamed: 0', axis=1, errors='ignore')
+    users.to_csv('users.csv', sep=';', encoding='windows-1251')
+
+
+def getpicture(user_id,ball,debt):
+    users = pd.read_csv('users.csv', sep=';', encoding='windows-1251', engine='python')
+    #teams = pd.read_csv('team.csv', sep=';', encoding='windows-1251', engine='python')
+    print(users.head())
+    users.loc[users['id'] == user_id, 'баллы'] += ball
+    if debt==False:
+        users.loc[users['id'] == user_id, 'дата'] = day_t()
+    else:
+        users.loc[users['id'] == user_id, 'дата'] -= 1
+    print(users.head())
+    users = users.drop('Unnamed: 0', axis=1, errors='ignore')
+    users.to_csv('users.csv', sep=';', encoding='windows-1251')
+
+
+
 
 
 def Counter():
     users = pd.read_csv('users.csv', sep=';', encoding='windows-1251', engine='python')
     teams = pd.read_csv('team.csv', sep=';', encoding='windows-1251', engine='python')
-    t=users.groupby(['команды']).agg({'баллы': 'sum', 'id': 'count' }).reset_index()
+    t=users.groupby(['команды']).agg({'баллы': 'sum', 'id': 'count'}).reset_index()
     for i in t['команды']:
         sum_balls = t[t['команды'] == i]['баллы']/day_t() / t[t['команды'] == i]['id']
         sum_balls = sum_balls.iloc[0]*100
@@ -43,3 +71,11 @@ def Counter():
         teams.loc[teams['им команды'] == i, 'баллы общие'] += team_ball
 
     print(teams.head())
+
+def Save(users, teams):
+    users=pd.read_excel(users, encoding='windows-1251')
+    teams = pd.read_excel(teams, encoding='windows-1251')
+    users = users.drop('Unnamed: 0', axis=1, errors='ignore')
+    users.to_csv('users.csv', sep=';', encoding='windows-1251')
+    teams = users.drop('Unnamed: 0', axis=1, errors='ignore')
+    teams.to_csv('team.csv', sep=';', encoding='windows-1251')
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import json
 import requests
-import wget #скачать её
-from Base import getpicture,Counter
+import wget
+from Base import getpicture, Counter, day_t, hour_t,Counter_d,Data,Debt,Save
+import os
 
 def get_button(label='but', color='red', payload=''):
     return {
@@ -14,14 +15,12 @@ def get_button(label='but', color='red', payload=''):
         },
         "color": color
     }
-# k=day_t() модуль работает, но бот тогда не работает)))
-# print(k)
 
 if __name__ == "__main__":
     token =
-    acess_token  =
+    acess_token =
     group_id = 191601892
-    album_id = 269997215
+    album_id = 270167491
     vk_message = vk_api.VkApi(token=token)
     vk_photo = vk_api.VkApi(token=acess_token)
     v = '5.103'#версия приложения
@@ -40,7 +39,7 @@ if __name__ == "__main__":
 
     def photo(user_id, url, group_id, album_id):
         a = vk_photo.method("photos.getMessagesUploadServer")
-        file = wget.download(url)  # сохраняем файл
+        file = wget.download(url) # сохраняем файл
         b = requests.post(a['upload_url'], files={'photo': open(file, 'rb')}).json()
         c = vk_photo.method('photos.saveMessagesPhoto', {'photo': b['photo'],
                                                    'server': b['server'],
@@ -57,12 +56,23 @@ if __name__ == "__main__":
                                   'aid': info['aid'],
                                   'hash': info['hash'], "album_id": album_id,
                                   'group_id': group_id})
+        os.remove(file)
 
     keyboard = str(json.dumps(keyboard, ensure_ascii=False))# переводим в строковый тип, так как не может принимать словарь
     longpoll = VkBotLongPoll(vk_message, group_id) #событие, наблюдаем за некоторой событийной моделью
     flag=False
     flag1=False
+    flag2=0
     for event in longpoll.listen():
+        if (hour_t() >= 1320) and (hour_t()<1380):
+            flag2=1
+        elif hour_t()>=1243:
+            flag2=2
+            album_id = vk_photo.method('photos.createAlbum', {'title': str(day_t()) + " День", 'group_id': group_id})['id']
+        else:
+            if flag2!=0:
+                Counter_d()
+                flag2=0
         if event.type == VkBotEventType.MESSAGE_NEW:
             if flag == True:
                 if event.object.message['attachments']:
@@ -77,7 +87,12 @@ if __name__ == "__main__":
                         id = event.object.message['peer_id']  # id отправителя
                         body = event.object.message['text']  # сам текст
                         photo(id, url, group_id, album_id)
-                        getpicture(event.object.message['peer_id'], 1)
+                        if flag2 == 0: #до 22 часов
+                            getpicture(event.object.message['peer_id'], 2, False)
+                        if flag2 == 1:# медлу 22-00 и 23-00
+                            getpicture(event.object.message['peer_id'], 1, False)
+                        if flag2 == 2:#опоздал
+                            getpicture(event.object.message['peer_id'], 3, False)
                         Counter()
                         flag = False
             if flag1 == True:
@@ -85,17 +100,18 @@ if __name__ == "__main__":
                     if event.object.message['attachments'][0]['type'] == 'photo':  # photo
                         vk_message.method('messages.send',
                                     {"peer_id": event.object.message['peer_id'],  # выведет ок если пришло фото
-                                    'attachment': 'photo-191561475_457239022',
-                                    'random_id': 0})
+                                        'attachment': 'photo-191561475_457239022',
+                                        'random_id': 0})
                         url = event.object.message['attachments'][0]['photo']['sizes'][5]['url']
                         messages = vk_message.method("messages.getConversations",
                                                 {'offset': 0, 'count': 20, 'filter': 'unread'})
                         id = event.object.message['peer_id']  # id отправителя
                         body = event.object.message['text']  # сам текст
                         photo(id, url, group_id, album_id)
-                        getpicture(event.object.message['peer_id'], 1)
+                        getpicture(event.object.message['peer_id'], 0.5, True)
                         Counter()
                         flag1 = False
+
             if event.object.message['text'].lower() == 'начать':# обратились к сообщению, образаемся к "тексту"
                 vk_message.method("messages.send", {"peer_id": event.object.message['peer_id'],
                                             'message': 'ok',
@@ -108,31 +124,39 @@ if __name__ == "__main__":
                                         'message': 'Привет, друг '+str(a),
                                         'random_id': 0})
             elif "Сдать работу" in event.object.message['text']:  # если нажать на кнопку, то проиходйет это
-                 flag=True
-
-                 vk_message.method('messages.send', {"peer_id": event.object.message['peer_id'],
+                 if Data(event.object.message['peer_id'])==day_t():
+                     vk_message.method('messages.send', {"peer_id": event.object.message['peer_id'],
+                                                         'message': "Вы уже сдавали работу! Ждём вас завтра!",
+                                                         'random_id': 0})
+                 else:
+                    flag=True
+                    vk_message.method('messages.send', {"peer_id": event.object.message['peer_id'],
                                              'message': "Загружай!",
                                              'random_id': 0})
             elif "Сдать долг!" in event.object.message['text']:  # если нажать на кнопку, то проиходйет это
-                 flag1=True
-
-                 vk_message.method('messages.send', {"peer_id": event.object.message['peer_id'],
+                if Debt(event.object.message['peer_id']) <=0:
+                    vk_message.method('messages.send', {"peer_id": event.object.message['peer_id'],
+                                                        'message': "Вы сдали все долги!",
+                                                        'random_id': 0})
+                else:
+                    flag1=True
+                    vk_message.method('messages.send', {"peer_id": event.object.message['peer_id'],
                                              'message': "Загружай долг!",
                                              'random_id': 0})
-
-
-            # elif event.object.message['attachments']:
-            #     if event.object.message['attachments'][0]['type'] == 'photo':        #photo
-            #         vk.method('messages.send', {"peer_id": event.object.message['peer_id'],#выведет ок если пришло фото
-            #                                     'attachment': 'photo-191561475_457239022',
-            #                                         'random_id': 0})
-
-                 #-------код что ниже, при запуске вырубает бота. Идёт ошибка
-                    # url = event.object.message['attachments'][0]['photo']['sizes'][5]['url']
-                    # messages = vk.method("messages.getConversations", {'offset': 0, 'count': 20, 'filter':'unread'})
-                    # id = event.object.message['peer_id'] #id отправителя
-                    # body = event.object.message['text']# сам текст
-                    # photo(id, url, group_id, album_id)
+            elif "Admin" in event.object.message['text']:  # если нажать на кнопку, то проиходйет это
+                if event.object.message['attachments'][0]['type'] == 'document':  #новая база
+                    url_users = event.object.message['attachments'][0]['document']['url']
+                    url_team  = event.object.message['attachments'][0]['document']['url']
+                    a = vk_photo.method("photos.getMessagesUploadServer") # поулчить стандалон для фото и документов
+                    file_users = wget.download(url_users)  # сохраняем файл
+                    file_team = wget.download(url_team)
+                    b = requests.post(a['upload_url'], files={'file': open(file, 'rb')}).json()
+                    c = vk_photo.method('photos.save', {'document': b["file"],
+                                                                     'server': b['server'],
+                                                                     'hash': b['hash']})[0]
+                    Save(file_users,file_team)
+                    os.remove(file_users)
+                    os.remove(file_team)
 
 
 
